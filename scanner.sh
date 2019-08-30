4538477ca049db414c745fdc05685297be5475f9
dsp976 <dsp976@gmail.com>
2019-08-30 08:58:22 -0400

    change service address

--- scanner.sh
+++ scanner.sh
@@ -4,6 +4,10 @@ while true
 do
 	rm -fr $GIT_LOCAL
 	git clone $GIT_REPO $GIT_LOCAL 
-	sonar-scanner -Dsonar.projectKey=$PROJECT_KEY -Dsonar.sources=$SRC
-	sleep 60
+	sonar-scanner \
+	  -Dsonar.projectKey=$PROJECT_KEY \
+	  -Dsonar.sources=$SRC \
+	  -Dsonar.host.url=$SONAR_HOST \
+	  -Dsonar.login=$SONAR_LOGIN
+	sleep 300
 done
